183947a7df864bb34c5ab666439d340b4db95d13
shittu adewale <sheriffhardewale@gmail.com>
2022-04-27 22:12:40 +0100

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-blue-400 p-4 h-20 flex ' >
+        <input type="text" name="" id="" />
+        <button>Search</button>
+    </div>
+  )
+}
+
+export default Header
