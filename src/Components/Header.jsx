dc8d6ec11a472098b592121df18947494b9161be
shittu adewale <sheriffhardewale@gmail.com>
2022-05-04 18:25:38 +0100

    changed background

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,7 +20,7 @@ const Header = ({word}) => {
   }
 
   return (
-    <div className="bg-gray-700">
+    <div className="bg-blue-400">
       <div className="container mx-auto py-8">
         <h1 className="text-3xl font-bold text-center text-white">Dictionary App</h1>
         
